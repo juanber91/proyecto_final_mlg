b9a008f69537db4896cbd8f4eba65d956d0f3d47
alexnivi <a.nivon.r@gmail.com>
2019-12-14 20:15:00 -0600

    Queretaro added

--- datos/Proyeccion de Poblacion/script_proyeccion_poblacion.R
+++ datos/Proyeccion de Poblacion/script_proyeccion_poblacion.R
@@ -2,13 +2,13 @@
 library(tidyverse)
 
 #### DATOS #####
-setwd("~/Documentos/RgrAvn/ProyectoFinal")
+setwd("~/Documentos/RgrAvn/ProyectoFinal/proyecto_final_mlg/datos")
 base <- read.csv("limpios.csv")
-pob_2005<-read.csv("pob_municipios_2005.csv") %>%
+pob_2005<-read.csv("Proyeccion de Poblacion/pob_municipios_2005.csv") %>%
   select(c('entidad', 'nom_ent', 'mun', 'nom_mun', 'p_total', 'prop_pob_mun'))
 names(pob_2005) <- c('cve_ent', 'nom_ent', 'cve_mun', 'mun', 'pob_ent_2005', 'prop_pob_ent_2005')
-  
-pob_2010<-read.csv("pob_municipios_2010.csv") %>% 
+
+pob_2010<-read.csv("Proyeccion de Poblacion/pob_municipios_2010.csv") %>% 
   select(c('entidad', 'nom_ent', 'mun', 'nom_mun', 'pobtot', 'prop_pob_mun'))
 names(pob_2010) <- c('cve_ent', 'nom_ent', 'cve_mun', 'mun', 'pob_ent_2010', 'prop_pob_ent_2010')
 
