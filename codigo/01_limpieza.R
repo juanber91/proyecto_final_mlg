016cc68de564c6ba88013f153ccc0a7aeb8bdee3
JBMPC <jbmpc@outlook.com>
2019-12-14 18:04:14 -0600

    EDA y tasas por estado/mes

--- codigo/01_limpieza.R
+++ codigo/01_limpieza.R
@@ -1,7 +1,7 @@
 library('tidyverse')
 library('magrittr')
 
-narco <- read.csv('Final/proyecto_final_mlg/datos/completa/EJECUCIONES (2006-2011).csv',
+narco <- read.csv('datos/completa/EJECUCIONES (2006-2011).csv',
                   sep = ';', encoding = 'latin1') %>% 
   mutate(AÑO_ID = as.character(AÑO_ID))
 
@@ -14,7 +14,7 @@ narco %>% str
 
 # Tablas externas ----_------------------------------------------
 
-tabla1 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla1-EJECUCIONES.xlsx') %>% 
+tabla1 <- readxl::read_excel('datos/completa/tabla1-EJECUCIONES.xlsx') %>% 
   setNames(c('AÑO_ID', 'VEHICLE')) %>% 
   mutate(aux = 1) %>% 
   filter(VEHICLE != 8) %>% 
@@ -22,15 +22,15 @@ tabla1 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla1-EJE
   setNames(c('AÑO_ID', 'v_automovil', 'v_camion', 'v_camioneta', 'v_jeep', 'v_moto', 'v_patrulla', 'v_tanque'))
 
 
-tabla2 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla1-EJECUCIONES.xlsx') %>% 
+tabla2 <- readxl::read_excel('datos/completa/tabla2-EJECUCIONES.xlsx') %>% 
   setNames(c('AÑO_ID', 'DRUGS')) %>% 
-  mutate(DRUGS = ifelse(DRUGS %in% c(1,3), DRUGS, 2)) %>%
+  mutate(DRUGS = ifelse(DRUGS %in% c(1,4), DRUGS, 2)) %>%
   count(AÑO_ID, DRUGS) %>% 
   mutate(n = 1) %>% 
   spread(DRUGS, n, 0) %>% 
-  setNames(c('AÑO_ID', 'd_cocaina', 'd_otras_drogas', 'd_extasis'))
+  setNames(c('AÑO_ID', 'd_cocaina', 'd_otras_drogas', 'd_marihuana'))
 
-tabla3 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla3-EJECUCIONES.xlsx') %>% 
+tabla3 <- readxl::read_excel('datos/completa/tabla3-EJECUCIONES.xlsx') %>% 
   setNames(c('AÑO_ID', 'AGE_RANGE')) %>% 
   filter(AGE_RANGE != 9999) %>% 
   mutate(aux = 1) %>% 
@@ -38,16 +38,16 @@ tabla3 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla3-EJE
   setNames(c('AÑO_ID', 'e_15_o_menos', 'e_15_a_20', 'e_20_a_30', 'e_30_a_50', 'e_50_o_mas'))
 
 
-tabla4 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla4-EJECUCIONES.xlsx')  %>% 
+tabla4 <- readxl::read_excel('datos/completa/tabla4-EJECUCIONES.xlsx')  %>% 
   setNames(c('AÑO_ID', 'WEAPON')) %>% 
   count(AÑO_ID) %>% 
   mutate(n = 1) %>% 
   rename(WEAPON = n)
 
-# tabla5 <- readxl::read_excel('Final/proyecto_final_mlg/datos/2010/table5-A-X10.xlsx') %>% 
+# tabla5 <- readxl::read_excel('datos/2010/table5-A-X10.xlsx') %>% 
 #   setNames(c('AÑO_ID', 'CALIBER'))
 
-tabla6 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla6-EJECUCIONES.xlsx') %>% 
+tabla6 <- readxl::read_excel('datos/completa/tabla6-EJECUCIONES.xlsx') %>% 
   setNames(c('AÑO_ID', 'MODALITY')) %>% 
   mutate(MODALITY = ifelse(MODALITY %in% c(1,4,11,5,15,8,20,6,12,14),
                            MODALITY, 9999)) %>% 
@@ -57,7 +57,7 @@ tabla6 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla6-EJE
              'm_mutiliado', 'm_encobijado', 'm_asfixiado', 'm_calcinado',
              'm_foza', 'm_golpes', 'm_tortura', 'm_vehiculo', 'm_otra'))
 
-tabla7 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla7-EJECUCIONES.xlsx') %>% 
+tabla7 <- readxl::read_excel('datos/completa/tabla7-EJECUCIONES.xlsx') %>% 
   setNames(c('AÑO_ID', 'MESSAGE_TYPE')) %>% 
   mutate(MESSAGE_TYPE = ifelse(MESSAGE_TYPE %in% 4:5, 'mens_narco',
                                 ifelse(MESSAGE_TYPE == 1, 'mens_gobierno',
@@ -66,7 +66,7 @@ tabla7 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla7-EJE
   count(AÑO_ID, MESSAGE_TYPE) %>% 
   spread(MESSAGE_TYPE, n, 0)
 
-tabla8 <- readxl::read_excel('Final/proyecto_final_mlg/datos/completa/tabla8-EJECUCIONES.xlsx') %>% 
+tabla8 <- readxl::read_excel('datos/completa/tabla8-EJECUCIONES.xlsx') %>% 
   setNames(c('AÑO_ID', 'CARTEL')) %>% 
   mutate(CARTEL = ifelse(CARTEL %in% c(20,14,190,18,86),
                            CARTEL, 9999)) %>% 
@@ -91,4 +91,4 @@ narco_full[is.na(narco_full)] <- 0
 narco_full[narco_full == 9999] <- NA
 
 data.frame(colSums(is.na(narco_full)))
-write.csv(narco_full, 'Final/proyecto_final_mlg/datos/limpios.csv')
+write.csv(narco_full, 'datos/limpios.csv')
---
+++ codigo/02_agregacion.R
@@ -0,0 +1,86 @@
+library(tidyverse)
+library(magrittr)
+library(R2jags)
+
+
+
+# Población -----------------------------------------------------
+
+pob <- read.csv('datos/Proyeccion de Poblacion/Población por Municipio 2005 - 2011.csv', )
+pob %<>% 
+  select(ESTADO = cve_ent, estado = nom_ent, cve_mun, contains('pob_ent')) %>% 
+  mutate(ESTADO = as.character(ESTADO)) %>% 
+  gather(AÑO, poblacion, contains('pob_ent')) %>% 
+  mutate(AÑO = parse_number(str_remove(AÑO, '\\.'))) %>% 
+  group_by(ESTADO, estado, AÑO) %>% 
+  summarise(poblacion = sum(poblacion)) %>% 
+  ungroup()
+
+pob
+
+
+# Narco ---------------------------------------------------------
+
+narco <- read.csv('datos/limpios.csv')
+
+narco %>% names
+narco %>% str
+
+narco %<>% mutate(FPE = is.na(FPE)) 
+
+narco_mensual <- narco %>%
+  filter(AÑO != 2006) %>% 
+  group_by(ESTADO, AÑO, MES) %>% 
+  summarise_at(vars(EJF:cartel_otro), ~sum(., na.rm = T)) %>% 
+  ungroup() %>% 
+  mutate(ESTADO = as.character(ESTADO))
+
+narco_mensual %>% count(ESTADO) %>% print(n=Inf)
+narco_mensual %>% head
+
+narco_mensual %<>% left_join(pob) %>% unique()
+narco_mensual %<>% mutate(tasa = EJ / poblacion * 1000000)
+narco_mensual$tasa %>% summary
+
+# EDA -----------------------------------------------------------
+
+# conteos por estado 
+
+estados <- narco_mensual %>% 
+  select(-AÑO, -MES) %>% 
+  group_by(ESTADO, estado) %>% 
+  summarise_all(~sum(., na.rm = T)) %>% 
+  ungroup() %>% 
+  select(-c(CORR, DRO, DTRA, EJM, EJF, ELE, FCRU, PRE, TAX, VEH, tasa))
+
+estados %>% summary
+
+plot_estados <- function(df, variable) {
+  
+  df %>% 
+    gather(var, val, -ESTADO, -estado) %>% 
+    filter(var == variable) %>% 
+    ggplot(aes(reorder(estado, val), val)) +
+    geom_col(aes(fill = factor(estado))) +
+    labs(title = variable, x='', y='') +
+    coord_flip() +
+    theme_light() +
+    theme(legend.position = 'none')
+  
+  ggsave(paste0('Final/proyecto_final_mlg/plots/',
+                variable, '.png'),
+         width = 6, height = 4)
+  
+}
+
+map(names(estados)[-c(1,2)], ~plot_estados(estados, .))
+
+narco_mensual %>% 
+  mutate(fecha = as.Date(paste(AÑO, str_pad(MES, 2, 'left', '0'), '01', sep = '-'))) %>% 
+  gather(var, val, -ESTADO, -estado, -AÑO, -MES, -fecha) %>% 
+  filter(var == 'tasa') %>% 
+  ggplot(aes(fecha, val)) +
+  geom_line(aes(color = factor(estado))) +
+  # labs(title = variable, x='', y='') +
+  theme_light() +
+  theme(legend.position = 'none')
